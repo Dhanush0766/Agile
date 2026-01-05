ee4e04bc90e318e2b174ddc7ae673df4732fdb36
Dhanush0766 <m7855988@gmail.com>
2026-01-05 11:18:14 +0530

    Add files via upload

---
+++ arm.py
@@ -0,0 +1,12 @@
+
+a = 10
+b = 3
+print(f"a = {a}")
+print(f"b = {b}\n")
+print(f"Addition (a + b): {a + b}")
+print(f"Subtraction (a - b): {a - b}") 
+print(f"Multiplication (a * b): {a * b}")
+print(f"Division (a / b): {a / b}")
+print(f"Floor Division (a // b): {a // b}") 
+print(f"Modulus (a % b): {a % b}") 
+print(f"Exponentiation (a ** b): {a ** b}") 
